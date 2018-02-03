1448b95bde6c2e87474b1c4bc196459b878178f8
kerwinBrant <2858427273@qq.com>
2018-02-03 16:09:38 +0800

    nav

---
+++ Zombie/Assets/NavMeshAgent/Nav.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Nav : MonoBehaviour {
+    private NavMeshAgent agent;
+    private Camera cam;
+    private Animator anim;
+    public Transform targetTrans;
+    public GameObject bullet;
+
+    public static float targetDistance = 15;
+
+    bool isBorn = false;
+
+    private int moveSpeedID = Animator.StringToHash("MoveSpeed");
+	// Use this for initialization
+	void Start () 
+    {
+        agent = GetComponent<NavMeshAgent>();
+        cam = GameObject.Find("Middle").GetComponent<Camera>();
+        anim = GetComponent<Animator>();
+
+        targetTrans = GameObject.FindWithTag("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        RaycastHit hit;
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            /*Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+                SetDestination(hit.point);*/
+            Born();
+   
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+            FindCar();
+        /* if (agent.enabled)
+         {
+             if (agent.remainingDistance != 0 && agent.remainingDistance <= 0.1f)
+             {
+                 agent.Stop();
+                 agent.enabled = false;
+                 anim.SetFloat(moveSpeedID, 0);
+             }
+             else
+                 anim.SetFloat(moveSpeedID, agent.remainingDistance);
+         }*/
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Attack();
+        }
+
+
+        if (!isBorn && targetTrans)
+        {
+            if (Vector3.Distance(transform.position, targetTrans.position) < targetDistance)
+            {
+                SelfBorn();
+            }
+        }
+
+    }
+    void SetDestination(Vector3 pos)
+    {
+        Debug.Log("SetDestination  =====   ");
+        agent.enabled = true;        
+        anim.SetFloat(moveSpeedID, 0.5f);
+        agent.SetDestination(pos);
+        agent.angularSpeed = 35f;
+        agent.speed = 0.3f;
+    }
+
+    public void Born()
+    {
+        if (Vector3.Distance(transform.position, targetTrans.position) < targetDistance)
+        {
+            Invoke("SelfBorn", Random.Range(0f, 2f));
+        }
+        
+    }
+
+    void SelfBorn()
+    {
+        anim.SetFloat(moveSpeedID, -2);
+        isBorn = true;
+    }
+
+
+    void FindCar()
+    {
+        SetDestination(targetTrans.position);
+    }
+
+    void Attack()
+    {
+        agent.enabled = false;
+        anim.enabled = false;
+        SetDestination(transform.position);
+
+    }
+
+}
---
+++ Zombie/Assets/NavMeshAgent/NavControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavControl : MonoBehaviour {
+
+    Nav[] zombies;
+    void Start()
+    {
+        zombies = FindObjectsOfType<Nav>();
+    
+    }
+
+    public void Born()
+    {
+        for (int i = 0; i < zombies.Length; i++)
+        {
+            zombies[i].Born();
+        }
+    }
+
+}
