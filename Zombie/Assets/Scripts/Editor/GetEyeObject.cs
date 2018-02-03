24b229eb56ff551c06fba3c6409a2b932558dd6d
kerwinBrant <2858427273@qq.com>
2018-02-03 15:35:10 +0800

    zom

--- Zombie/Assets/Scripts/Editor/GetEyeObject.cs
+++ Zombie/Assets/Scripts/Editor/GetEyeObject.cs
@@ -12,5 +12,16 @@ public class GetEyeObject : Editor {
         Selection.activeGameObject = obj;
     }
 
+    [MenuItem("工具/生成僵尸")]
+    static void GenereteZom()
+    {
+        GameObject obj = GameObject.FindObjectOfType<Nav>().gameObject;
+        for (int i = 0; i < 50; i++)
+        {
+            GameObject go = GameObject.Instantiate(obj);
+            go.transform.position = new Vector3(i * 0.5f, 0, i * 0.5f);
+        }
+    }
+
 
 }
